bd612a6d9cbcc008e61ac6433669d6d7d38fc707
ledinhkhuong132006 <24DH110883@st.huflit.edu.vn>
2025-10-12 23:46:27 +0700

    add file demo

---
+++ Đồ án/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tranggiaohang.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult trangchu()
+        {
+            return View();
+        }
+        public ActionResult trangdangnhap()
+        {
+            return View();
+        }
+        public ActionResult tranghome()
+        {
+            return View();
+        }
+        public ActionResult trangsanpham1()
+        {
+            return View();
+        }
+        public ActionResult trangsanpham2()
+        {
+            return View();
+        }
+        public ActionResult tranggiohang()
+        {
+            return View();
+        }
+        public ActionResult tranggiao()
+        {
+            return View();
+        }
+        public ActionResult trangthanhtoan()
+        {
+            return View();
+        }
+        public ActionResult trangxacnhan()
+        {
+            return View();
+        }
+        public ActionResult tranglichsu()
+        {
+            return View();
+        }
+    }
+}
