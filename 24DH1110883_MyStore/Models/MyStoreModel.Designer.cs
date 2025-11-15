796d6c35077541e742471bcafb549a4428aa3ba0
ledinhkhuong132006 <24DH110883@st.huflit.edu.vn>
2025-11-15 21:16:48 +0700

    add file demo

--- 24DH1110883_MyStore/Models/MyStoreModel.Designer.cs
+++ 24DH1110883_MyStore/Models/MyStoreModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\BE\24DH1110883_MyStore\Models\MyStoreModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\nop bai git\24DH1110883_MyStore\Models\MyStoreModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
