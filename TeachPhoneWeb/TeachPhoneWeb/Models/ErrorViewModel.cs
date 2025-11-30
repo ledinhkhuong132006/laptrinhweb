3677c8a8521837947ab7b22df79404c5189a1d7c
ledinhkhuong132006 <24DH110883@st.huflit.edu.vn>
2025-12-01 00:31:16 +0700

    Delete TeachPhoneWeb directory

--- TeachPhoneWeb/TeachPhoneWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TeachPhoneWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
